f9fbfab140d01587ca9f5cf423c7a38d38f39431
Zack Blaylock <zackblaylock@gmail.com>
2018-02-26 21:43:04 -0500

    Added the heroku base api url.

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+//export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://lost-n-found-server.herokuapp.com';
+
