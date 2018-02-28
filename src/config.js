a34bb491e30f361fc1492ca417ca57a14a44ce1d
Zack Blaylock <zackblaylock@gmail.com>
2018-02-27 21:43:29 -0500

    Completed linking Landing and Dashboard pages with no errors and no duplicate redirects.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 //export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
 
-//export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://lost-n-found-server.herokuapp.com';
+export const API_BASE_URL = 'reverent-goldwasser-c5f288.netlify.com';
+// process.env.REACT_APP_API_BASE_URL || 'https://lost-n-found-server.herokuapp.com';
 
